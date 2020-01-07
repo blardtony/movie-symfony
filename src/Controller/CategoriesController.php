7867b865b2b03dc829a217c99cad63f30ea72569
Tony Blard <tonyblard55@gmail.com>
2020-01-07 10:00:14 +0100

    Authors card done and genre done too

---
+++ src/Controller/CategoriesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Categories;
+
+class CategoriesController extends AbstractController
+{
+    /**
+     * @Route("/categories/{{id}}", name="categories")
+     */
+    public function show($id)
+    {
+      $category = $this->getDoctrine()->getRepository(Categories::class)->find($id);
+      return $this->render('categories/index.html.twig', [
+        'category' => $category,
+      ]);
+  }
+}
