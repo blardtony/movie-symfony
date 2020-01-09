031ba34b5f166fbf07b96d1234541386401371fe
Tony Blard <tonyblard55@gmail.com>
2020-01-09 11:00:54 +0100

    testing

---
+++ tests/acceptance/UserCest.php
@@ -0,0 +1,28 @@
+<?php namespace App\Tests;
+use App\Tests\AcceptanceTester;
+
+class UserCest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function tryToSuccessLogin(AcceptanceTester $I)
+    {
+      //$I->amOnPage('/login');
+      //$I->fillField('email', 'tonyblard55@gmail.com');
+      //$I->fillField('password', 'alaji');
+      //$I->click('Se connecter');
+      //$I->see('Vous êtes connecté avec le nom tonyblard55@gmail.com');
+      //$I->makeHtmlSnapshot();
+    }
+    public function tryToFailLogin(AcceptanceTester $I)
+    {
+      $I->amOnPage('/login');
+      $I->fillField('email', 'cool@gmail.com');
+      $I->fillField('password', 'alaji');
+      $I->click('Se connecter');
+      $I->see('Email could not be found');
+    }
+}
