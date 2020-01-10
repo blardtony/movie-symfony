1937d68116bc49bb20f7ba7a147e6219e967a889
Tony Blard <tonyblard55@gmail.com>
2020-01-10 15:29:27 +0100

    ok

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -4,6 +4,10 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+
+use Symfony\Component\HttpFoundation\Request;
+use Knp\Component\Pager\PaginatorInterface;
+
 use App\Entity\Categories;
 
 class CategoriesController extends AbstractController
@@ -11,14 +15,21 @@ class CategoriesController extends AbstractController
     /**
      * @Route("/categories/{id}", name="categories")
      */
-    public function show($id)
+    public function show(Request $request, PaginatorInterface $paginator, $id)
     {
       $category = $this->getDoctrine()->getRepository(Categories::class)->find($id);
+
       if (!$category) {
         return $this->redirectToRoute('movies_list');
       }
+      $categories = $paginator->paginate(
+        $category, // Requête contenant les données à paginer (ici nos articles)
+        $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+        10 // Nombre de résultats par page
+      );
+
       return $this->render('categories/index.html.twig', [
-        'category' => $category,
+        'category' => $categories,
       ]);
   }
 }
