80c7b74f40cee6e2497479c7f1142b58b01d9ee4
Tony Blard <tonyblard55@gmail.com>
2020-01-13 12:00:35 +0100

    indentation

--- src/Controller/CategoriesController.php
+++ src/Controller/CategoriesController.php
@@ -12,26 +12,26 @@ use App\Entity\Categories;
 
 class CategoriesController extends AbstractController
 {
-    /**
-     * @Route("/categories/{id}", name="categories")
-     */
-    public function show(Request $request, PaginatorInterface $paginator, $id)
-    {
-      $category = $this->getDoctrine()->getRepository(Categories::class)->find($id);
+  /**
+   * @Route("/categories/{id}", name="categories")
+   */
+  public function show(Request $request, PaginatorInterface $paginator, $id)
+  {
+    $category = $this->getDoctrine()->getRepository(Categories::class)->find($id);
 
-      if (!$category) {
-        return $this->redirectToRoute('movies_list');
-      }
-      $pagination = $paginator->paginate(
-        $category->getMovies(), // Requête contenant les données à paginer (ici nos articles)
-        $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
-        18 // Nombre de résultats par page
-      );
-
-      return $this->render('categories/index.html.twig', [
-        'categories' => $pagination,
-        'category' => $category,
-      ]);
-
+    if (!$category) {
+      return $this->redirectToRoute('movies_list');
     }
+    $pagination = $paginator->paginate(
+      $category->getMovies(), // Requête contenant les données à paginer (ici nos articles)
+      $request->query->getInt('page', 1), // Numéro de la page en cours, passé dans l'URL, 1 si aucune page
+      18 // Nombre de résultats par page
+    );
+
+    return $this->render('categories/index.html.twig', [
+      'categories' => $pagination,
+      'category' => $category,
+    ]);
+
+  }
 }
